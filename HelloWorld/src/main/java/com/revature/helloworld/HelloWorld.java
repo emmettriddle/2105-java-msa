f40a39689c18c59c0bacc3ecb083ab2d93669efe
Daniel Felleman <daniel.felleman@revature.com>
2021-05-12 15:34:44 -0400

    adding Java notes and code

---
+++ HelloWorld/src/main/java/com/revature/helloworld/HelloWorld.java
@@ -0,0 +1,76 @@
+
+// This is how we write a comment in Java
+
+// This line (package) must always be the first line in a .java file (non-commented)
+package com.revature.helloworld;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Multi Line Comment
+ * 
+ * Packages are a way of organizing our code into a hierarchy
+ * 
+ * lower.case.naming.convention
+ * 
+ * 2 types of packages:
+ * 
+ * 
+ * User-Defined (like this one that we're working in). 
+ * 
+ * Built-In
+ * 
+ * java.lang (automatically imported)
+ * 
+ * Other built in packages (Collections) - but these need to be manually imported.
+ * 
+ * DRY - Don't Repeat Yourself
+ * 
+ * */
+//import java.util.logging.*;
+
+/*
+ * Class Declaration - PascaleCase
+ * */
+
+/**
+ * You can create your own javadocs to share pertinent information 
+ * with other developers who might need to work on your code. 
+ * 
+ * String name = Dan
+ * 
+ * @author DanielFelleman
+ *
+ */
+public class HelloWorld {
+	
+	
+	// main method - it acts as an entry point into our program.
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello World!");
+
+		 
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
